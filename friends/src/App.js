8b004f09792ded061e0280036eb4d997ac89ffd9
mogwai <feelirabs@gmail.com>
2020-08-20 23:42:55 +0100

    Flurry of CSS

--- friends/src/App.js
+++ friends/src/App.js
@@ -5,21 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h2>Log in to the kini</h2>
+      <form>
+        <input type="text" placeholder="Log in"/>
+        <br/>
+        <input type="password" placeholder="Enter password"/>
+        <br/>
+        <button>Smash me</button>
+      </form>
+      </div>
   );
 }
 
