8d06e4d5cbc951166b6777124885d2ad67ab843b
mogwai <feelirabs@gmail.com>
2020-08-21 21:34:32 +0100

    adding state management

--- friends/src/Components/Login.js
+++ friends/src/Components/Login.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 
 function Login() {
+  const [credentials, setCredentials] = useState({})
+  const handleSubmit = (e)=> {
+    e.preventDefault()
+    setCredentials({...credentials})
+  }
+  const handleChange =(e)=> {
+    console.log(e.target.name)
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+    console.log(credentials)
+
+  }
   return (
     <div className="App">
       <h2>Log in to the kini</h2>
-      <form>
-        <input type="text" placeholder="Log in"/>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name='username' value={credentials.username} onChange={handleChange} placeholder="Log in"/>
         <br/>
-        <input type="password" placeholder="Enter password"/>
+        <input type="password" name='password' value={credentials.password} onChange={handleChange} placeholder="Enter password"/>
         <br/>
         <button>Smash me</button>
       </form>
