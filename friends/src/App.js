580c21d29d2ebe943db638142ff4963821a1da1b
mogwai <feelirabs@gmail.com>
2020-08-21 14:07:22 +0100

    Set up router doms

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Login from './Login';
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 
 const App = ()=> {
     return (
-    <div>
-        <Login/>
+    <Router>
+        <Route exact path="/">
+            <h1>Welcome eh?</h1>
+            <Link to="/login">Go to login</Link>
+        </Route>
+        <Route path="/login"><Login/></Route>
 
-    </div>
+    </Router>
     )
 }
 
