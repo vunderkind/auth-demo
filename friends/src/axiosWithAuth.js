7e8020fe471f21d570ad89d390b56b2af01c142c
mogwai <feelirabs@gmail.com>
2020-08-21 14:49:30 +0100

    demo commit

--- friends/src/axiosWithAuth.js
+++ friends/src/axiosWithAuth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const axiosWithAuth = ()=> {
     const token = localStorage.getItem('token');
-
+//bullshit comment for demo purposes
     return axios.create({
         headers: {
             Authorization: token
