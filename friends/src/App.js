bed020d947e4fdfe34a7d8eea1dc611e2b4184d4
mogwai <feelirabs@gmail.com>
2020-08-21 14:04:28 +0100

    Created Login dedicated route

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './Login';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Log in to the kini</h2>
-      <form>
-        <input type="text" placeholder="Log in"/>
-        <br/>
-        <input type="password" placeholder="Enter password"/>
-        <br/>
-        <button>Smash me</button>
-      </form>
-      </div>
-  );
+const App = ()=> {
+    return (
+    <div>
+        <Login/>
+
+    </div>
+    )
 }
 
-export default App;
+export default App
---
+++ friends/src/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Log in to the kini</h2>
+      <form>
+        <input type="text" placeholder="Log in"/>
+        <br/>
+        <input type="password" placeholder="Enter password"/>
+        <br/>
+        <button>Smash me</button>
+      </form>
+      </div>
+  );
+}
+
+export default App;
